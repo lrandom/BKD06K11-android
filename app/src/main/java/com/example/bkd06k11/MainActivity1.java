ec756e9ca69db377f72c31098441ae56549f3a0e
Nguyen Thanh Luan <luann4099@gmail.com>
2021-11-17 19:14:59 +0700

    update demo activity for result

--- app/src/main/java/com/example/bkd06k11/MainActivity1.java
+++ app/src/main/java/com/example/bkd06k11/MainActivity1.java
@@ -1,16 +1,25 @@
 package com.example.bkd06k11;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity1 extends AppCompatActivity {
     public static final String TAG_NAME = "NAME";
-    Button btnStartActivity, btnOpenLink;
+    public static final int REQUEST_CODE = 100;
+    public static final int REQUEST_CODE_2 = 101;
+    Button btnStartActivity, btnOpenLink, btnStartActivityForResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +54,7 @@ public class MainActivity1 extends AppCompatActivity {
             public void onClick(View v) {
                 Uri uri = Uri.parse("https://google.com.vn");
 
-           /*     Intent intent = new Intent(Intent.ACTION_VIEW, uri);*/
+                /*     Intent intent = new Intent(Intent.ACTION_VIEW, uri);*/
 
                 Intent intent = new Intent();
                 intent.setAction(Intent.ACTION_VIEW);
@@ -53,5 +62,50 @@ public class MainActivity1 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        ActivityResultLauncher<Intent> startActivity2ForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Intent data = result.getData();
+                            String name = data.getStringExtra("NAME");
+                            Log.i("RESULT", name);
+                        }
+                    }
+                }
+        );
+
+
+        btnStartActivityForResult = findViewById(R.id.btnStartaForResult);
+        btnStartActivityForResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                //startActivityForResult(intent, REQUEST_CODE);
+                startActivity2ForResult.launch(intent);
+            }
+        });
+
+
     }
+
+
+/*    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    String name = data.getStringExtra("NAME");
+                    Log.i("RESULT", name);
+                }
+                break;
+
+            case REQUEST_CODE_2:
+                break;
+        }
+    }*/
 }
--- app/src/main/java/com/example/bkd06k11/MainActivity2.java
+++ app/src/main/java/com/example/bkd06k11/MainActivity2.java
@@ -1,12 +1,16 @@
 package com.example.bkd06k11;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button btnReturnResult;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,16 +19,29 @@ public class MainActivity2 extends AppCompatActivity {
 /*        String name = getIntent().getStringExtra("NAME");
         String address = getIntent().getStringExtra("ADDRESS");*/
 
-        Bundle bundle = getIntent().getExtras();
+       /* Bundle bundle = getIntent().getExtras();
         String name1 = bundle.getString(MainActivity1.TAG_NAME);
-        String address1 = bundle.getString("ADDRESS");
+        String address1 = bundle.getString("ADDRESS");*/
 
 /*
         Log.i("name", name);
         Log.i("address", address);
 */
 
-        Log.i("name", name1);
-        Log.i("address", address1);
+        /*Log.i("name", name1);
+        Log.i("address", address1);*/
+
+
+        btnReturnResult = findViewById(R.id.btnReturnResult);
+        btnReturnResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("NAME", "Nguyen Thanh Luan");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+
+        });
     }
 }
