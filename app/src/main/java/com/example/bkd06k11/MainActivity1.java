07c10b813eda08c20046002c070bf272a85076d9
Nguyen Thanh Luan <luann4099@gmail.com>
2021-11-19 18:35:58 +0700

    session4

--- app/src/main/java/com/example/bkd06k11/MainActivity1.java
+++ app/src/main/java/com/example/bkd06k11/MainActivity1.java
@@ -1,21 +1,26 @@
 package com.example.bkd06k11;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity1 extends AppCompatActivity {
     public static final String TAG = "MainActivity1";
+    String name = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
         Log.i(TAG, "onCreate");
-        ;
+
+
     }
 
 
@@ -58,5 +63,30 @@ public class MainActivity1 extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "onDestroy");
+    }
+
+    public void getDataOneTime(View view) {
+        EditText edtName = findViewById(R.id.edtName);
+        this.name = edtName.getText().toString();//lấy dữ liệu từ edit text
+    }
+
+    public void printName(View view) {
+        System.out.println("Name Value " + this.name);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("name",this.name);//lưu lại
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getString("name") != null) {
+                this.name = savedInstanceState.getString("name");
+            }
+        }
     }
 }
