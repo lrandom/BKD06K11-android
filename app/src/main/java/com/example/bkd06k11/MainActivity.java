665bf47cb71a2d69d4b169090058795c9bc85072
Nguyen Thanh Luan <luann4099@gmail.com>
2021-11-26 18:13:21 +0700

    update session 6 demo

--- app/src/main/java/com/example/bkd06k11/MainActivity.java
+++ app/src/main/java/com/example/bkd06k11/MainActivity.java
@@ -8,40 +8,27 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
+
+        System.out.println("hello");
+
+        test2();
     }
 
-    public void openWebPage(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vnexpress.vn"));
-       // intent.addCategory(Intent.CATEGORY_BROWSABLE);
-        // if (intent.resolveActivity(getPackageManager()) != null) {
-        try {
-            startActivity(intent);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        //}
-    }
 
-    public void callPhone(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:012384567"));
-        startActivity(intent);
-    }
-
-    public void sendSMS(View v) {
-        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:012435678"));
-        //intent.putExtra("sms_body", "Hello");
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+    public void test() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
         }
     }
 
-    public void openMap(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:21.04451419793164, 105.82012212627899"));
-        startActivity(intent);
+    public void test2() {
+        for (int j = 0; j < 5; j++) {
+            System.out.println(j);
+        }
     }
 }
