6e80b7de5b4fe5a8bd410a65b3cab7e83a466250
Nguyen Thanh Luan <luann4099@gmail.com>
2021-12-01 18:16:59 +0700

    add unit test

---
+++ app/src/main/java/com/example/bkd06k11/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.bkd06k11;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
--- app/src/test/java/com/example/bkd06k11/ExampleUnitTest.java
+++ app/src/test/java/com/example/bkd06k11/ExampleUnitTest.java
@@ -1,5 +1,9 @@
 package com.example.bkd06k11;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +14,50 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("before");
+        calculator = new Calculator();
+    }
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        System.out.println("beforeClass");
+    }
+
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        System.out.println("test");
+        assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    public void sub_isCorrect() {
+        System.out.println("test");
+        assertEquals(2, calculator.sub(4, 2));
+    }
+
+    @Test
+    public void equalArr() {
+        assertArrayEquals(new int[]{1,2,3},new int[]{1,2,3});
+    }
+
+    @Test
+    public void equalException() {
+        assertThrows(Exception.class, () -> {
+            Integer.parseInt("1");
+        });
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        System.out.println("afterClass");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("after");
     }
 }
