61eb9b80f8c73acce3894ea0e203fcc6619d479b
Nguyen Thanh Luan <luann4099@gmail.com>
2021-12-15 19:05:20 +0700

    handle click recyclerview

--- app/src/main/java/com/example/bkd06k11/RecyclerViewActivity.java
+++ app/src/main/java/com/example/bkd06k11/RecyclerViewActivity.java
@@ -5,7 +5,10 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.bkd06k11.adapters.AdapterCountry;
 import com.example.bkd06k11.domains.Country;
@@ -25,11 +28,19 @@ public class RecyclerViewActivity extends AppCompatActivity {
             countries.add(new Country("Country " + i));
         }
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(RecyclerViewActivity.this, LinearLayoutManager.VERTICAL,true);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(RecyclerViewActivity.this,3,LinearLayoutManager.VERTICAL,false);
-        rcCountry.setLayoutManager(gridLayoutManager);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(RecyclerViewActivity.this, LinearLayoutManager.VERTICAL, true);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(RecyclerViewActivity.this, 3, LinearLayoutManager.VERTICAL, false);
+        rcCountry.setLayoutManager(linearLayoutManager);
         AdapterCountry adapterCountry = new AdapterCountry(RecyclerViewActivity.this, countries);
-        rcCountry.setAdapter(adapterCountry);
+        adapterCountry.setOnItemClickListener(new AdapterCountry.MyItemClickListener() {
+            @Override
+            public void itemClick(View ew, int position) {
+                Toast.makeText(RecyclerViewActivity.this,
+                        countries.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
+
+        rcCountry.setAdapter(adapterCountry);
     }
 }
--- app/src/main/java/com/example/bkd06k11/adapters/AdapterCountry.java
+++ app/src/main/java/com/example/bkd06k11/adapters/AdapterCountry.java
@@ -1,15 +1,20 @@
 package com.example.bkd06k11.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bkd06k11.R;
+import com.example.bkd06k11.RecyclerViewActivity;
+import com.example.bkd06k11.Session10;
 import com.example.bkd06k11.domains.Country;
 
 import java.util.ArrayList;
@@ -18,6 +23,8 @@ public class AdapterCountry extends RecyclerView.Adapter<AdapterCountry.MyViewHo
     Context context;
     LayoutInflater layoutInflater;
     ArrayList<Country> countries = new ArrayList<>();
+    public static String TAG = "AdapterCountry";
+    public MyItemClickListener myItemClickListener;
 
     public AdapterCountry(Context context, ArrayList<Country> countries) {
         this.context = context;
@@ -44,12 +51,29 @@ public class AdapterCountry extends RecyclerView.Adapter<AdapterCountry.MyViewHo
         return this.countries.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public interface MyItemClickListener{
+        public void itemClick(View view, int position);
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
         TextView tvName;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             this.tvName = itemView.findViewById(R.id.tvName);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Intent intent = new Intent(RecyclerViewActivity.this, Session10.class);
+                    //context.startActivity(intent);
+                    Log.d(TAG, "onClick: ");
+                    myItemClickListener.itemClick(itemView,getAdapterPosition());
+                }
+            });
         }
+    }
+
+    public void setOnItemClickListener(MyItemClickListener clickListener) {
+        this.myItemClickListener = clickListener;
     }
 }
