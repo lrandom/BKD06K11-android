cb5cfa878feebc4a97f4b2b8c312a23134dff047
Nguyen Thanh Luan <luann4099@gmail.com>
2021-12-17 18:00:54 +0700

    update demo

--- app/src/main/java/com/example/bkd06k11/adapters/AdapterCountry.java
+++ app/src/main/java/com/example/bkd06k11/adapters/AdapterCountry.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,6 +47,36 @@ public class AdapterCountry extends RecyclerView.Adapter<AdapterCountry.MyViewHo
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         Country country = countries.get(position);
         holder.tvName.setText(country.getName());
+        holder.btnOpenMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setTag(country);
+                _showPopupMenu(v);
+            }
+        });
+    }
+
+    private void _showPopupMenu(View v) {
+        Country country = (Country) v.getTag();
+        PopupMenu popupMenu = new PopupMenu(context, v);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+        //xử lý khi tap vào item menu
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item_menu_1:
+                        Toast.makeText(context, "Bạn đã chọn xoá"+country.getName(), Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.item_menu_2:
+                        Toast.makeText(context, "Bạn đã chọn sửa"+country.getName(), Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
     }
 
     @Override
@@ -51,23 +84,25 @@ public class AdapterCountry extends RecyclerView.Adapter<AdapterCountry.MyViewHo
         return this.countries.size();
     }
 
-    public interface MyItemClickListener{
+    public interface MyItemClickListener {
         public void itemClick(View view, int position);
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView tvName;
+        ImageButton btnOpenMenu;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             this.tvName = itemView.findViewById(R.id.tvName);
+            this.btnOpenMenu = itemView.findViewById(R.id.btnOpenMenu);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     //Intent intent = new Intent(RecyclerViewActivity.this, Session10.class);
                     //context.startActivity(intent);
                     Log.d(TAG, "onClick: ");
-                    myItemClickListener.itemClick(itemView,getAdapterPosition());
+                    myItemClickListener.itemClick(itemView, getAdapterPosition());
                 }
             });
         }
