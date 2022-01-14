6255f7fac150dad9072983b3d1e17ac2bcebed25
Nguyen Thanh Luan <luann4099@gmail.com>
2022-01-14 19:11:44 +0700

    update session-15

---
+++ app/src/main/java/com/example/bkd06k11/activities/ActivitySecond.java
@@ -0,0 +1,16 @@
+package com.example.bkd06k11.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bkd06k11.R;
+
+public class ActivitySecond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
