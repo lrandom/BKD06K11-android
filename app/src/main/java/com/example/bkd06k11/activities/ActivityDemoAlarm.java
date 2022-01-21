81977ede7df039558d99598e6fe80b8cd030759f
Nguyen Thanh Luan <luann4099@gmail.com>
2022-01-21 18:28:34 +0700

    demo jobschedular

--- app/src/main/java/com/example/bkd06k11/activities/ActivityDemoAlarm.java
+++ app/src/main/java/com/example/bkd06k11/activities/ActivityDemoAlarm.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -17,46 +20,30 @@ import com.example.bkd06k11.R;
 
 public class ActivityDemoAlarm extends AppCompatActivity {
     Button btnSetAlarm;
-    public static final String ACTION_ALARM_WAKEUP = "bk.com.ACTION_ALARM_WAKEUP";
+    public static final int JOB_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_alarm);
         btnSetAlarm = findViewById(R.id.btnSetAlarm);
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         btnSetAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                long currentTime = System.currentTimeMillis();//lấy về timestamp tính từ 1-1-1970 OO:OO:0000 (ra đời Unix)
-                Intent intent = new Intent(ACTION_ALARM_WAKEUP);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(ActivityDemoAlarm.this, 1,
-                        intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                //alarmManager.setExact(AlarmManager.RTC_WAKEUP, currentTime + 2000, pendingIntent);
-                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, currentTime + 2000,
-                        60000,
-                        pendingIntent);
+                JobInfo.Builder jobInfoBuilder = new JobInfo.Builder(JOB_ID,
+                        new ComponentName(getPackageName(), MyJobService.class.getName())
+                );
+                jobInfoBuilder
+                        .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
 
-                //ANDROID 8 // trạng thái Doze (0 cắm sạc, đt ở chế độ tắt màn hình)
-                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,currentTime+2000,
-                        pendingIntent);
-            }/**/
+                JobInfo jobInfo = jobInfoBuilder.build();
+
+                JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+                jobScheduler.schedule(jobInfo);
+            }
         });
 
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(ACTION_ALARM_WAKEUP);
-        registerReceiver(new MyReceiver(), intentFilter);
     }
 
 
-    class MyReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            switch (intent.getAction()) {
-                case ACTION_ALARM_WAKEUP:
-                    Toast.makeText(getApplicationContext(), "Báo thức bật", Toast.LENGTH_SHORT).show();
-                    break;
-            }
-        }
-    }
 }
